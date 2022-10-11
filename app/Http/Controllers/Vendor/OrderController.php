bf4c6ae45924a6e37903586f378f1a1c13c49064
neelesh2210 <nelesh2210@gmail.com>
2022-10-11 22:34:30 +0530

    order

--- app/Http/Controllers/Vendor/OrderController.php
+++ app/Http/Controllers/Vendor/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Vendor;
 
+use Auth;
 use App\Models\Vendor\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -24,7 +25,35 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $or_id = Order::latest()->first();
+        if($or_id)
+        {
+            $order_id = explode('-',$or_id->order_id)[1]+1;
+        }
+        else
+        {
+            $order_id = 100001;
+        }
+
+        $order = new Order;
+        $order->order_id = 'ORD-'.$order_id;
+        $order->user_id = Auth::guard('vendor')->user()->id;
+        $order->branch_id = $request->branch_id;
+        $order->destination_id = $request->destination_id;
+        $order->receiver_name = $request->receiver_name;
+        $order->receiver_phone = $request->receiver_phone;
+        $order->receiver_alt_phone = $request->receiver_alt_phone;
+        $order->receiver_address = $request->receiver_address;
+        $order->weight = $request->weight;
+        $order->delivery_charge = $request->delivery_charge;
+        $order->pickup_type = $request->pickup_type;
+        $order->cod_charge = $request->cod_charge;
+        $order->package_access = $request->package_access;
+        $order->package_type = $request->package_type;
+        $order->remark = $request->remark;
+        $order->save();
+
+        return redirect()->route('orders.index')->with('success','Order Created Successfully!');
     }
 
     public function show($id)
--- resources/views/vendor/order/create.blade.php
+++ resources/views/vendor/order/create.blade.php
@@ -19,13 +19,14 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <form class="needs-validation" novalidate>
+                        <form action="{{route('orders.store')}}" class="needs-validation" novalidate method="POST">
+                            @csrf
                             <div class="row">
                                 <div class="col-md-6 pt-2">
                                     <label class="form-label" for="branch_id">Branch</label>
                                     <select name="branch_id" id="branch_id" class="form-control" required>
                                         @foreach (App\Models\Admin\Branch::orderBy('branch_name','asc')->get() as $branch)
-                                            <option value="{{$branch->id}}" selected disabled>{{$branch->branch_name}}</option>
+                                            <option value="{{$branch->id}}" selected>{{$branch->branch_name}}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -44,7 +45,7 @@
                                 </div>
                                 <div class="col-md-6 pt-2">
                                     <label class="form-label" for="receiver_phone">Receiver Phone</label>
-                                    <input type="text" name="receiver_phone" id="receiver_phone" class="form-control" placeholder="Receiver Phone...">
+                                    <input type="number" name="receiver_phone" id="receiver_phone" class="form-control" placeholder="Receiver Phone...">
                                 </div>
                                 <div class="col-md-6 pt-2">
                                     <label class="form-label" for="receiver_alt_phone">Receiver Alt Phone</label>
@@ -71,7 +72,7 @@
                                 </div>
                                 <div class="col-md-6 pt-2">
                                     <label class="form-label" for="cod_charge">COD Charge</label>
-                                    <input type="number" step="0.01" name="cod_charge" id="cod_charge" class="form-control" value="1" placeholder="COD Charge...">
+                                    <input type="number" step="0.01" name="cod_charge" id="cod_charge" class="form-control" placeholder="COD Charge...">
                                 </div>
                                 <div class="col-md-6 pt-2">
                                     <label class="form-label" for="package_access">Package Access</label>
