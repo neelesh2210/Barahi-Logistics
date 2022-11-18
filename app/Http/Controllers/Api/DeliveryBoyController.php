01f39b1f258ab6a345efc6ace2c7306763601515
neelesh2210 <43535202+neelesh2210@users.noreply.github.com>
2022-11-18 16:56:38 +0530

    Update DeliveryBoyController.php

--- app/Http/Controllers/Api/DeliveryBoyController.php
+++ app/Http/Controllers/Api/DeliveryBoyController.php
@@ -12,11 +12,6 @@ class DeliveryBoyController extends Controller
 
     public function deliveryBoyLogin(Request $request)
     {
-        $validatedData = $request->validate([
-            'phone_number' => 'required|string|min:10|max:10',
-            'password' => 'required|string|min:8',
-        ]);
-        return 1;
         $delivery_boy = DeliveryBoy::where('phone_number',$request->phone_number)->first();
 
         if($delivery_boy)
