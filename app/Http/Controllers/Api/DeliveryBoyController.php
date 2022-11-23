da06f2fdf11904e447436d6b1651d36952719ca9
neelesh2210 <nelesh2210@gmail.com>
2022-11-23 20:31:14 +0530

    status change api and timezone

--- app/Http/Controllers/Api/DeliveryBoyController.php
+++ app/Http/Controllers/Api/DeliveryBoyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Vendor\Order;
 use Illuminate\Http\Request;
 use App\Models\Admin\AssignOrder;
 use App\Models\Admin\DeliveryBoy;
@@ -39,6 +40,20 @@ class DeliveryBoyController extends Controller
         return $assign_orders = AssignOrder::where('delivery_boy_id',Auth::user()->id)->with('order')->paginate(10);
 
         return response()->json(['assign_orders'=>$assign_orders]);
+    }
+
+    public function deliveryBoyStatusChange(Request $request)
+    {
+        $order = Order::find($request->order_id);
+        $order->order_status = $request->status;
+
+        $order_status_date=json_decode($order->order_status_date);
+        array_push($order_status_date,[$request->status=>date('d-m-y H:i')]);
+
+        $order->order_status_date = $order_status_date;
+        $order->save();
+
+        return response()->json(['msg'=>'Order Status Changed Successfully!']);
     }
 
 }
